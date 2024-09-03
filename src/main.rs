60a9e738835543d2412b44b4e88702605b54f305
motiongorilla <nize.afonya@gmail.com>
2024-09-03 20:29:31 +0100

    updated to hold additional methods

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,26 @@ struct Rectangle {
     height: f32,
 }
 
+impl Rectangle {
+    fn area(&self) -> f32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, another_rect: &Rectangle) -> bool {
+        self.width > another_rect.width && self.height > another_rect.height
+    }
+}
+
 fn main() {
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         width: 73.0,
         height: 35.0,
     };
+    let rect2 = Rectangle {
+        width: 34.0,
+        height: 48.0,
+    };
 
-    println!("The area is {}", rect_area(&rect));
-}
-
-fn rect_area(rect: &Rectangle) -> f32 {
-    rect.width * rect.height
+    println!("The area is {}", rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
